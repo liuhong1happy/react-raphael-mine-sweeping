5b2f836e7869d175134b74e5eed530d5671c7245
Holly Liu <hollyliu@HollydeMac-mini.local>
2016-11-30 20:18:01 +0800

    add mark

--- src/box.js
+++ src/box.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const {Set,Rect,Image,Text} = require('react-raphael');
+const {Set,Rect,Image,Text,Path} = require('react-raphael');
 
 var colors = {
 	"": "#000",
@@ -16,10 +16,17 @@ var colors = {
 }
 
 class Box extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            mark: false
+        }
+    }
 	shouldComponentUpdate(nextProps,nextState){
 		var fOpen = this.props.open != nextProps.open;
 		var fValue = this.props.value != nextProps.value;
-		return fOpen || fValue;
+        var fMark = this.state.mark != nextState.mark;
+		return fOpen || fValue || fMark;
 	}
 	handleClick(e){
 		var {x,y,onClick} = this.props;
@@ -27,11 +34,21 @@ class Box extends React.Component{
 			onClick(x,y);
 		}
 	}
+    handleMouseDown(e){
+        e = e || event;
+        var button = e.button;
+        if(button != 2) return;
+        this.setState({
+             mark: !this.state.mark
+        })
+    }
 	render(){
 		var {x,y,width,height,value,open} = this.props;
 		if(!open){
 			return (<Set>
-					<Rect x={x*width} y={y*height} width={width} height={height} attr={{"fill":"#7a9afa","stroke":"#fff","stroke-width":1 }} click={this.handleClick.bind(this)} />
+					<Rect x={x*width} y={y*height} width={width} height={height} attr={{"fill":"#7a9afa","stroke":"#fff","stroke-width":1 }} click={this.handleClick.bind(this)} mousedown={this.handleMouseDown.bind(this)}/>
+                    <Path d={["M",x*width+10, y*height+4,"L", x*width+10, y*height +height-4 ]} attr={{"fill":"yellow","stroke":"yellow","stroke-width":2}} hide={!this.state.mark}/>
+                    <Path d={["M",x*width+12, y*height+4,"L", x*width+width-4, y*height +(height-8)/4,"L", x*width+12, y*height +(height-8)/2 ,"Z"]} attr={{"fill":"red","stroke":"#333"}} hide={!this.state.mark}/>
 				</Set>)
 		}else if(value.length>1){
 			return (<Set>
